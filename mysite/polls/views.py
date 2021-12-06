f86846a1aafd426df957237c367550c89162add4
Ravi Shankar <ravi.kumar4@squareyards.co.in>
2021-12-06 15:35:40 +0530

    Added index() in polls/views.py

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -2,8 +2,24 @@ from django.shortcuts import render
 
 # Create your views here.
 
-from django.http import HttpResponse
+from django.http import HttpResponse, response
+
+
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    output = ', '.join([q.question_text for q in latest_question_list])
+    return HttpResponse(output)
 
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+def detail(request, question_id):
+    return HttpResponse("You are looking at questions %s." % question_id)
+
+def results(request, question_id):
+    response = "You are looking at the result of the question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return("You're voting on question %s." % question_id)
